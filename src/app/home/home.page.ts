a8e0d5c441f1a2354e5d23d95b1582a004f1aa8f
RafaelMoraes08 <rafamoraes5550@gmail.com>
2020-05-27 16:44:23 -0300

    Mapa criado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+
+declare var google: any;
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,25 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  map: any;
+
+  @ViewChild('map', {read: ElementRef, static:false}) mapRef: ElementRef;
+
+  constructor() { }
+
+  
+  public showMap() {
+    const location = new google.maps.LatLng(-22.526850, -48.566584);
+    const options = {
+      center: location,
+      zoom:  15,
+      disableDefaultUI: true
+    }
+    this.map = new google.maps.Map(this.mapRef.nativeElement, options)
+  }
+
+  ionViewDidEnter(){
+    this.showMap();
+  }
 
 }
