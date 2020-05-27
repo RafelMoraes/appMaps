b190be16ec4c9d28dd0f19150e4197dbb5d6d90c
RafaelMoraes08 <rafamoraes5550@gmail.com>
2020-05-27 18:21:04 -0300

    adicionado o marcador

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 declare var google: any;
 
@@ -10,24 +11,50 @@ declare var google: any;
 export class HomePage {
 
   map: any;
+  posicaoAtual: any;
 
   @ViewChild('map', {read: ElementRef, static:false}) mapRef: ElementRef;
 
-  constructor() { }
+  constructor(private geolocation: Geolocation) { }
 
   
-  public showMap() {
-    const location = new google.maps.LatLng(-22.526850, -48.566584);
+  public async showMap() {
+     // posição fixa const location = new google.maps.LatLng(-22.526850, -48.566584);
+
+     await this.buscaPosicao();
+
     const options = {
-      center: location,
+      center: this.posicaoAtual,
       zoom:  15,
       disableDefaultUI: true
     }
     this.map = new google.maps.Map(this.mapRef.nativeElement, options)
+
+    const marcador = new google.maps.Marker({
+      position: this.posicaoAtual,
+      map: this.map,
+      title: "Minha localização",
+      icon: "https://maps.google.com/mapfiles/ms/icons/green-dot.png",
+      animation: google.maps.Animation.DROP
+    });
+
   }
 
   ionViewDidEnter(){
     this.showMap();
+  }
+
+  public async buscaPosicao(){
+   await this.geolocation.getCurrentPosition().then((posicaoGPS) => {
+      this.posicaoAtual = {
+        lat: posicaoGPS.coords.latitude,
+        lng: posicaoGPS.coords.longitude
+      }
+
+     }).catch((error) => {
+       console.log('Error getting location', error);
+     });
+
   }
 
 }
