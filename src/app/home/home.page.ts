68beaeb6c60f8e27f2a7f512201e7b9810d04d10
RafaelMoraes08 <rafamoraes5550@gmail.com>
2020-05-29 20:26:24 -0300

    Adicionado os maracadores

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { Ilocal } from '../Intefaces/Ilocal';
 
 declare var google: any;
 
@@ -12,7 +13,40 @@ export class HomePage {
 
   map: any;
   posicaoAtual: any;
-  posicaoB: any;
+  
+  public listaLocais: Ilocal[]=[
+    {
+      lat: -22.524791, 
+      lng: -48.566125,
+      titulo:'Lanchonete Do Chefia'
+    },
+    {
+      lat: -22.525158,  
+      lng: -48.567480,
+      titulo:'Ginásio Arlindo Fadoni'
+    },
+    {
+      lat: -22.520817, 
+      lng: -48.565527, 
+      titulo:'Barbearia Imperido do corte'
+    },
+    {
+      lat: -22.517929, 
+      lng: -48.554781,
+      titulo:'Ricardinho Eltricista'
+    },
+    {
+      lat: -22.516156,  
+      lng: -48.539435,
+      titulo:'Prainha de Igaraçu'
+    },
+    
+    {
+      lat: -22.526850,  
+      lng: -48.566584,
+      titulo:'Minha posição'
+    }
+  ];
 
   @ViewChild('map', {read: ElementRef, static:false}) mapRef: ElementRef;
 
@@ -39,11 +73,26 @@ export class HomePage {
       animation: google.maps.Animation.DROP
     });
 
+    for(let local of this.listaLocais){
+      this.adicionarMarc(local);
+    }
+
   }
 
   ionViewDidEnter(){
     this.showMap();
-    this.marcB();
+  }
+
+  private adicionarMarc(Local: Ilocal) {
+    const {lat, lng, titulo} = Local;
+
+    const marcador = new google.maps.Marker({
+      position: {lat, lng},
+      map: this.map,
+      title: titulo,
+      icon: "https://maps.google.com/mapfiles/ms/icons/green-dot.png"
+    });
+    
   }
 
   public async buscaPosicao(){
@@ -62,29 +111,4 @@ export class HomePage {
   /**
    * posicaoB
    */
-  public async marcB() {
-    const posicaoB = new google.maps.LatLng(-22.525034, -48.567559);
-    await this.buscaPosicaoB();
-
-    const marcador = new google.maps.Marker({
-      position: this.posicaoB,
-      map: this.map,
-      title: "Localização B",
-      icon: "https://maps.google.com/mapfiles/ms/icons/green-dot.png",
-      animation: google.maps.Animation.DROP
-    });
-  }
-
-  public async buscaPosicaoB(){
-    await this.geolocation.getCurrentPosition().then((posicaoB) => {
-       this.posicaoB = {
-         lat: posicaoB.coords.latitude,
-         lng: posicaoB.coords.longitude
-       }
- 
-      }).catch((error) => {
-        console.log('Error getting location', error);
-      });
-
-}
 }
